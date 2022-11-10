5cca83db03e6008d84ed476ae15b4f89a82d9013
Anna <vdovinanna1991@gmail.com>
2022-11-10 15:28:53 +0300

    PrintArray

--- Finish_HW/Program.cs
+++ Finish_HW/Program.cs
@@ -14,3 +14,28 @@ void FillArray(string[] symbols1, string[] symbols2)
         }
 }
 }
+string[] symbols1 = new string[5] {"157", "2", "hello", "world", ";)"};
+string[] symbols2 = new string[symbols1.Length];
+
+
+void FillArray(string[] symbols1, string[] symbols2)
+{
+    int count = 0;
+    for (int i = 0; i < symbols1.Length; i++)
+    {
+    if(symbols1[i].Length <= 3)
+        {
+        symbols2[count] = symbols1[i];
+        count++;
+        }
+}
+}
+
+void PrintArray(string[] symbols)
+{
+    for (int i = 0; i < symbols.Length; i++)
+    {
+       Console.Write($"{symbols[i]} ");
+    }
+    Console.WriteLine();
+}
