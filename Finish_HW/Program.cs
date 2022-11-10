cfacc60f4bc5a000fb0883704a499b4ccc666f62
Anna <vdovinanna1991@gmail.com>
2022-11-10 15:25:35 +0300

    FillArray

---
+++ Finish_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] symbols1 = new string[5] {"157", "2", "hello", "world", ";)"};
+string[] symbols2 = new string[symbols1.Length];
+
+
+void FillArray(string[] symbols1, string[] symbols2)
+{
+    int count = 0;
+    for (int i = 0; i < symbols1.Length; i++)
+    {
+    if(symbols1[i].Length <= 3)
+        {
+        symbols2[count] = symbols1[i];
+        count++;
+        }
+}
+}
